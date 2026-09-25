3e82e460d4b08b570ca346ce05f78d1194d36f49
NyxMods000 <nyxmodsbitch@gmail.com>
2026-09-24 23:18:13 -0500

    Actualizar Loader.lua

--- Mm2/Loader.lua
+++ Mm2/Loader.lua
@@ -1,1 +1,25 @@
-uwu
+local Url = "https://raw.githubusercontent.com/NyxMods000/NyxModsUniversal/refs/heads/main/Mm2/Source.lua"
+
+local Request = (syn and syn.request) or http_request or (http and http.request)
+
+if Request then
+ local Response = Request({
+  Url = Url,
+  Method = "GET",
+ })
+
+if Response then
+ local Code = Response and Response.Body
+ 
+ pcall(function()
+  loadstring(Code)()
+  return
+ end)
+ 
+ if Code then
+  return
+ end
+end
+end
+
+loadstring(game:HttpGet(Url))()
